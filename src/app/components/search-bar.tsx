6b4467294eccf24633b202bdb120e0842d542bec
SOWAIBA NAZ <167256849+sowaibathecoder@users.noreply.github.com>
2024-12-16 23:55:06 +0500

    search-bar.tsx

--- src/app/components/search-bar.tsx
+++ src/app/components/search-bar.tsx
@@ -18,15 +18,17 @@ export default function SearchBar() {
             height={24}
             className="absolute top-[10px] left-5"
           />
-          <div className="w-[149px] h-[20px] absolute top-[12px] left-16 font-['Plus_Jakarta_Sans'] text-sm font-medium leading-[21px] tracking-[-0.02em] text-justify text-[#596780]">
-            Search something here
-          </div>
+          <input 
+            type="text" 
+            placeholder="Search something here" 
+            className="w-[149px] h-[20px] absolute top-[12px] left-16 font-['Plus_Jakarta_Sans'] text-sm font-medium leading-[21px] tracking-[-0.02em] text-[#596780] placeholder-[#596780] outline-none"
+          />
           <Image
             src="/filter.png"
             alt="filter-icon"
             width={24}
             height={24}
-            className="absolute top-[10px] left-[445px]"
+            className="absolute top-[10px] left-[445px] cursor-pointer"
           />
         </div>
         <div className="w-[295px] h-[44px] absolute top-[40px] left-[1100px] gap-[20px] flex cursor-pointer">
